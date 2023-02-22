94ae87531900b6761fbf1fca27934806ac3b5f43
Maikpwwq <mariasf@correo.udistrital.edu.co>
2023-02-22 18:34:05 -0500

    fix

--- netlify/functions/get_mongoose/get_mongoose.js
+++ netlify/functions/get_mongoose/get_mongoose.js
@@ -11,12 +11,12 @@ const DB_HOST = `${process.env.VITE_DB_HOST}`;
 const DB_NAME = `${process.env.VITE_DB_NAME}`;
 const MONGODB_COLLECTION = `${process.env.VITE_MONGODB_COLLECTION}`;
 
-const MONGO_HOST = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority`;
+const MONGO_HOST = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority`;
 
-const schema = new mongoose.Schema({ name: 'string', access: 'string' });
+const schema = new mongoose.Schema({ name: "string", access: "string" });
 
 console.log("MONGOOSE_HOST", MONGO_HOST);
-const clientPromise = mongoose.createConnection(MONGO_HOST, {
+const clientPromise = await mongoose.createConnection(MONGO_HOST, {
   useUnifiedTopology: true,
   useNewUrlParser: true,
 });
@@ -30,18 +30,30 @@ const handler = async () => {
   try {
     //await
     //mongoClient.connect( async (err) => {
-    const database = await clientPromise;
-    console.log("[db] Mongoose Conection", database);
-    const collection = database.db(DB_NAME).collection(MONGODB_COLLECTION);
-    console.log("[db] Conectada con éxito", collection, MONGODB_COLLECTION);
-    const results = await collection.find().toArray();
-    if (results.length > 0) {
-      console.log("mongoClient", results);
-      return {
-        statusCode: 200,
-        body: JSON.stringify(results),
-      };
-    }
+    Contactos.find({}, function (err, docs) {
+      // docs.forEach
+      console.log("docs", docs);
+      const results = docs
+      if (results.length > 0) {
+        console.log("mongoClient", results);
+        return {
+          statusCode: 200,
+          body: JSON.stringify(results),
+        };
+      }
+    });
+    // const database = await clientPromise;
+    // console.log("[db] Mongoose Conection", database);
+    // const collection = database.db(DB_NAME).collection(MONGODB_COLLECTION);
+    // console.log("[db] Conectada con éxito", collection, MONGODB_COLLECTION);
+    // const results = await collection.find().toArray();
+    // if (results.length > 0) {
+    //   console.log("mongoClient", results);
+    //   return {
+    //     statusCode: 200,
+    //     body: JSON.stringify(results),
+    //   };
+    // }
   } catch (err) {
     console.error("[db] Error", MONGO_HOST, err);
     return { statusCode: 500, body: err.toString() };
@@ -52,4 +64,4 @@ const handler = async () => {
   //   }
 };
 
-export { mongoClient, handler };
+export { clientPromise, handler };
