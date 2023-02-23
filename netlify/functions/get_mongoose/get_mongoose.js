b3e78f4616a965c07bd8b0c7ed01ea07cb0eddff
Maikpwwq <mariasf@correo.udistrital.edu.co>
2023-02-22 19:29:46 -0500

    fix

--- netlify/functions/get_mongoose/get_mongoose.js
+++ netlify/functions/get_mongoose/get_mongoose.js
@@ -11,7 +11,7 @@ const DB_HOST = `${process.env.VITE_DB_HOST}`;
 const DB_NAME = `${process.env.VITE_DB_NAME}`;
 const MONGODB_COLLECTION = `${process.env.VITE_MONGODB_COLLECTION}`;
 
-const MONGO_HOST = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}${DB_NAME}?retryWrites=true&w=majority`;
+const MONGO_HOST = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority`;
 
 const schema = new mongoose.Schema({ name: "string", email: "string" });
 
